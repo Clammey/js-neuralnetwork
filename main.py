7b472b8daed987a0179ef76071b4df7c208a3510
Clayton Johnson <claytonjohnsoniscool@gmail.com>
2024-07-11 16:10:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #import packages
 
 import cv2
-from cvzone.HandTrackingModule import HandDetector 
 
 face_classifier = cv2.CascadeClassifier( # Face Detection model
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
